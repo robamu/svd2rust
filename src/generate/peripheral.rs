bb135a000d899ff4f756b52fb307b2881dc1b37a
Robin Mueller <robin.mueller.m@gmail.com>
2021-11-09 14:23:29 +0100

    using upstream implementation for ercs generation

--- src/generate/peripheral.rs
+++ src/generate/peripheral.rs
@@ -135,46 +135,12 @@ pub fn render(
 
                 match (erc, ancestor) {
                     (RegisterCluster::Register(reg), RegisterCluster::Register(other_reg)) => {
-                        // Check all possible combinations for derived and own type
-                        match (reg, other_reg) {
-                            (Register::Single(_), Register::Single(ref other_info)) => {
-                                Some(RegisterCluster::Register(Register::Single(
-                                    reg.derive_from(other_info),
-                                )))
-                            }
-                            (
-                                Register::Single(_),
-                                Register::Array(ref other_info, ref other_dim),
-                            ) => Some(RegisterCluster::Register(Register::Array(
-                                reg.derive_from(other_info),
-                                other_dim.clone(),
-                            ))),
-                            (Register::Array(_, ref dim), Register::Single(ref other_info)) => {
-                                Some(RegisterCluster::Register(Register::Array(
-                                    reg.derive_from(other_info),
-                                    dim.clone(),
-                                )))
-                            }
-                            (
-                                Register::Array(_, _),
-                                Register::Array(ref other_info, ref other_dim),
-                            ) => Some(RegisterCluster::Register(Register::Array(
-                                reg.derive_from(other_info),
-                                other_dim.clone(),
-                            ))),
-                        }
+                        Some(RegisterCluster::Register(reg.derive_from(other_reg)))
                     }
                     (
                         RegisterCluster::Cluster(cluster),
                         RegisterCluster::Cluster(other_cluster),
-                    ) => match other_cluster {
-                        Cluster::Array(ref info, ref array_info) => Some(RegisterCluster::Cluster(
-                            Cluster::Array(cluster.derive_from(info), array_info.clone()),
-                        )),
-                        Cluster::Single(ref info) => Some(RegisterCluster::Cluster(
-                            Cluster::Single(cluster.derive_from(info)),
-                        )),
-                    },
+                    ) => Some(RegisterCluster::Cluster(cluster.derive_from(other_cluster))),
                     _ => {
                         eprintln!(
                             "{} can't derive from {}",
